fe150b7f897566a796cb0720974b37f1a0a49e9d
Irtiza Ashraf <137706794+IrtizaAshraf@users.noreply.github.com>
2024-02-18 20:53:56 +0500

    done

---
+++ index.js
@@ -0,0 +1,21 @@
+// console.log("hello world in the Backend");
+
+const express = require('express')
+const app = express()
+const port = 3000
+
+
+app.get('/', (req, res)=>{
+    res.send('Home')
+})
+
+app.get('/store', (req, res)=>{
+    res.send('Store')
+})
+app.get('/store/product', (req, res)=>{
+    res.send('Products')
+})
+
+app.listen(port,()=>{
+    console.log(`hellow ${port}`);
+})
